188672996ce5a6d0df394a516d42566d82893909
onkar <onkar@gmail.com>
2023-12-11 18:57:35 +0530

    Add 2 packages

---
+++ src/com/java/controller/controller.java
@@ -0,0 +1,10 @@
+package com.java.controller;
+
+public class controller {
+
+	public static void main(String[] args) {
+		System.out.println("Controller");
+
+	}
+
+}
---
+++ src/com/java/service/service.java
@@ -0,0 +1,11 @@
+package com.java.service;
+
+public class service {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Service");
+
+	}
+
+}
